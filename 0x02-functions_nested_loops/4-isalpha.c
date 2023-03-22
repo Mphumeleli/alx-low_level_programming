41fca6c8ac77903ff493f1ab06ed086cd8573e35
mphumeleli <mphumelelizuma4@gmail.com>
2023-03-22 13:21:03 -0700

    alphas:
    git commit -m alphas:

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,13 @@
 #include "main.h"
-
 /**
- * isalpha - checks for alphabetic character
- * @c: the character to be checked
- * Return: 1 if c is a letter , 0 otherwise
+ * _isalpha - a function that checks for alphabetic character
+ * @c: single letter input
+ * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
  */
-
-int isalpha(int c)
+int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-	{
-	return (1);
-	}
-	return (0);
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+		return (1);
+	else
+		return (0);
 }
